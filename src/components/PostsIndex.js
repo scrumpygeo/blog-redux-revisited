89ff959902057af9979a3ec1c695b8228f6fa0fe
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-28 10:44:43 +0100

    wire up actionCreator in Postsindex

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
     return <div>PostsIndex</div>;
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
