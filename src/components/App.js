ce945b18596c9e183a49d75ef02c3263cd919301
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-28 10:36:55 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostsIndex from './PostsIndex';
+
+const App = () => {
+  return (
+    <div className='ui container'>
+      <PostsIndex />
+    </div>
+  );
+};
+
+export default App;
