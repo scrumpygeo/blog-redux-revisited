591dba535c0a39a32c3973d335cb17650e73e36e
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-28 13:31:59 +0100

    list posts rendered

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -7,9 +7,31 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        // jsx
+        <div className='item' key={post.id}>
+          <i className='large middle aligned icon user' />
+          <div className='content'>
+            <div className='description'>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>PostsIndex</div>;
+    // console.log(this.props);
+    return <div className='ui relaxed divided list'>{this.renderList()}</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
