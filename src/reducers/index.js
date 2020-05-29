0f03d2c5fb1901beb4ca5c16de24a0d02bde1900
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-29 11:55:59 +0100

    wire up redux-form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import postsIndexReducer from './postsIndexReducer';
 
 export default combineReducers({
   posts: postsIndexReducer,
+  form: formReducer,
 });
