d4c6adcf3424275e4b03b15ab8d2cd49e6953252
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-29 14:33:41 +0100

    wire up submit button

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostCreate extends Component {
-  renderInput = ({ input, label }) => {
+  renderInput({ input, label }) {
     return (
       <div className='field'>
         <label>{label}</label>
         <input {...input} />
       </div>
     );
-  };
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
 
   render() {
     return (
-      <form className='ui form'>
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form'
+      >
         <Field name='title' component={this.renderInput} label='Title' />
         <Field
           name='categories'
@@ -21,6 +28,8 @@ class PostCreate extends Component {
           label='Category'
         />
         <Field name='content' component={this.renderInput} label='Content' />
+
+        <button className='button ui primary'>Submit</button>
       </form>
     );
   }
