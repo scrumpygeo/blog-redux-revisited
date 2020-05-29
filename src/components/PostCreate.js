8894b38ac277de0b837e15c742efdb764aaee2f1
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-29 15:19:30 +0100

    add validation to form

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostCreate extends Component {
-  renderInput({ input, label }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
     return (
-      <div className='field'>
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete='off' />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {
     console.log(formValues);
@@ -19,7 +30,7 @@ class PostCreate extends Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className='ui form'
+        className='ui form error'
       >
         <Field name='title' component={this.renderInput} label='Title' />
         <Field
@@ -35,6 +46,22 @@ class PostCreate extends Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    // runs only if user doesn't enter a title
+    errors.title = 'You must enter a title.';
+  }
+
+  if (!formValues.content) {
+    errors.content = 'You must enter some content.';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: 'postCreate',
+  validate,
 })(PostCreate);
