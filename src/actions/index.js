878cea05ca6a47d9690a0d4d2db727df937dfbb1
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-29 20:16:17 +0100

    create types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,20 @@
 import reduxBlog from '../apis/reduxBlog';
+import { FETCH_POSTS, CREATE_POSTS, FETCH_POST } from './types';
 
 // NB: due to thunk we call the dispatch function manually with the action we want to dispatch.
 // instead of the classic return {  type: 'FETCH_POSTS', payload: promise }
 export const fetchPosts = () => async (dispatch) => {
   const response = await reduxBlog.get(`posts`);
   // console.log(response);
-  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+  dispatch({ type: FETCH_POSTS, payload: response.data });
 };
 
 export const fetchPost = (id) => async (dispatch) => {
   const response = await reduxBlog.get(`/posts/${id}`);
 
-  dispatch({ type: 'FETCH_POST', payload: response.data });
+  dispatch({ type: FETCH_POST, payload: response.data });
 };
 
 export const createPost = (formValues) => async (dispatch) => {
-  reduxBlog.post('/posts', formValues);
+  const response = reduxBlog.post('/posts', formValues);
 };
---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POSTS = 'CREATE_POSTS';
+export const FETCH_POST = 'FETCH_POST';
