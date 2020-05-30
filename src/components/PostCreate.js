8ddc3aa48a37c135109a7f79bc78457768484fc6
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-30 19:41:14 +0100

    refactor PostCreate to use PostForm

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -1,76 +1,21 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createPost } from '../actions';
+import PostForm from './PostForm';
 
 class PostCreate extends Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className='ui error message'>
-          <div className='header'>{error}</div>
-        </div>
-      );
-    }
-  }
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete='off' />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
   onSubmit = (formValues) => {
     this.props.createPost(formValues);
   };
 
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className='ui form error'
-      >
-        <Field name='title' component={this.renderInput} label='Title' />
-        <Field
-          name='categories'
-          component={this.renderInput}
-          label='Category'
-        />
-        <Field name='content' component={this.renderInput} label='Content' />
-
-        <button className='button ui primary'>Submit</button>
-      </form>
+      <div>
+        <h3>Make a Post</h3>
+        <PostForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-
-  if (!formValues.title) {
-    // runs only if user doesn't enter a title
-    errors.title = 'You must enter a title.';
-  }
-
-  if (!formValues.category) {
-    // runs only if user doesn't enter a title
-    errors.category = 'You must enter a category.';
-  }
-
-  if (!formValues.content) {
-    errors.content = 'You must enter some content.';
-  }
-
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: 'postCreate',
-  validate,
-})(PostCreate);
-
-export default connect(null, { createPost })(formWrapped);
+export default connect(null, { createPost })(PostCreate);
