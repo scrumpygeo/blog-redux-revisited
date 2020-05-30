91143efbc1d7dba6a99710cb271c533987757255
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-30 19:15:18 +0100

    add componentdidmount to postedit and wire up

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 ## React-Redux Blog
 
 Simple CRUD react redux app using heroku blog backend.
+
+1. Setup
+
+Using `http://reduxblog.herokuapp.com/api/posts` api for blogs which has these fields:
+
+```
+
+title
+categories
+content
+
+eg
+
+{
+id: 319494,
+title: "Merhaba/Hello",
+categories: "request, response",
+content: "Bu bir denemedir./This is just a test."
+},
+```
--- src/components/PostEdit.js
+++ src/components/PostEdit.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
-const PostEdit = () => {
-  return <div>PostEdit</div>;
+class PostEdit extends Component {
+  componentDidMount() {
+    this.props.fetchPost(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.post) {
+      return <div>Loading...</div>;
+    }
+
+    return <div>{this.props.post.title}</div>;
+  }
+}
+const mapStateToProps = (state, ownProps) => {
+  return { post: state.posts[ownProps.match.params.id] };
 };
 
-export default PostEdit;
+export default connect(mapStateToProps, { fetchPost })(PostEdit);
