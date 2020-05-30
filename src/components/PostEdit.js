e3ff7e584d93aff763b5b2927c73052de0daff95
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-30 20:08:28 +0100

    wire up edit with form

--- src/components/PostEdit.js
+++ src/components/PostEdit.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { fetchPost, editPost } from '../actions';
+import PostForm from './PostForm';
 
 class PostEdit extends Component {
   componentDidMount() {
     this.props.fetchPost(this.props.match.params.id);
   }
+
+  onSubmit = (formValues) => {};
+
   render() {
     if (!this.props.post) {
       return <div>Loading...</div>;
     }
 
-    return <div>{this.props.post.title}</div>;
+    return (
+      <div>
+        <h3>Edit Post</h3>
+        <PostForm initialValues={this.props.post} onSubmit={this.onSubmit} />
+      </div>
+    );
   }
 }
 const mapStateToProps = (state, ownProps) => {
   return { post: state.posts[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchPost })(PostEdit);
+export default connect(mapStateToProps, { fetchPost, editPost })(PostEdit);
