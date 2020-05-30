6ce91e5188f48194d2d39111e900318fa2e44e08
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-30 10:00:18 +0100

    add fetch post, edit post and create post reducers

--- src/reducers/postsIndexReducer.js
+++ src/reducers/postsIndexReducer.js
@@ -1,9 +1,19 @@
-export default (state = [], action) => {
+import {
+  FETCH_POSTS,
+  FETCH_POST,
+  CREATE_POST,
+  EDIT_POST,
+  DELETE_POST,
+} from '../actions/types';
+
+export default (state = {}, action) => {
   switch (action.type) {
-    case 'FETCH_POSTS':
-      return action.payload;
-    case 'FETCH_POST':
-      return action.payload;
+    case FETCH_POST:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_POST:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_POST:
+      return { ...state, [action.payload.id]: action.payload };
     default:
       return state;
   }
