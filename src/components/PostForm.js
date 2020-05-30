dede9179fdeae8efc16ef1e8428c2754b4db18be
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-30 19:32:02 +0100

    create generalized postform component

---
+++ src/components/PostForm.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class FormCreate extends Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete='off' />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form error'
+      >
+        <Field name='title' component={this.renderInput} label='Title' />
+        <Field
+          name='categories'
+          component={this.renderInput}
+          label='Category'
+        />
+        <Field name='content' component={this.renderInput} label='Content' />
+
+        <button className='button ui primary'>Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    // runs only if user doesn't enter a title
+    errors.title = 'You must enter a title.';
+  }
+
+  if (!formValues.category) {
+    // runs only if user doesn't enter a title
+    errors.category = 'You must enter a category.';
+  }
+
+  if (!formValues.content) {
+    errors.content = 'You must enter some content.';
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'postForm',
+  validate,
+})(FormCreate);
