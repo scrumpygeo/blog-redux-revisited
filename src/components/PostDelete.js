3d49a3b972ea3ee9ecb822abf838c6a87a93b3b6
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-31 09:29:33 +0100

    refactor postdelete and delete modal

--- src/components/PostDelete.js
+++ src/components/PostDelete.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Modal from '../components/Modal';
+import history from '../history';
 
 const PostDelete = () => {
+  const actions = (
+    <Fragment>
+      <button className='ui button negative'>Delete</button>
+      <button className='ui button'>Cancel</button>
+    </Fragment>
+  );
+
   return (
     <div>
       PostDelete
-      <Modal />
+      <Modal
+        title='Delete Post'
+        content='Are you sure you want to delete this post?'
+        actions={actions}
+        onDismiss={() => {
+          history.push('/');
+        }}
+      />
     </div>
   );
 };
