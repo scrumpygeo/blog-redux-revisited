a76777e863094d268863f9b4be98cd3cb5fab895
scrumpygeo <scrumpygeo@yahoo.ca>
2020-05-31 10:07:49 +0100

    modify modal messag

--- src/components/PostDelete.js
+++ src/components/PostDelete.js
@@ -1,28 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import { connect } from 'react-redux';
 import Modal from '../components/Modal';
 import history from '../history';
+import { fetchPost } from '../actions';
 
-const PostDelete = () => {
-  const actions = (
-    <Fragment>
-      <button className='ui button negative'>Delete</button>
-      <button className='ui button'>Cancel</button>
-    </Fragment>
-  );
+class PostDelete extends Component {
+  componentDidMount() {
+    this.props.fetchPost(this.props.match.params.id);
+  }
 
-  return (
-    <div>
-      PostDelete
+  renderActions() {
+    return (
+      <Fragment>
+        <button className='ui button negative'>Delete</button>
+        <button className='ui button'>Cancel</button>
+      </Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.post) {
+      return 'Are you sure you want to delete this post?';
+    }
+    return `Are you sure you want to delete the post with title: ${this.props.post.title}`;
+  }
+
+  render() {
+    return (
       <Modal
         title='Delete Post'
-        content='Are you sure you want to delete this post?'
-        actions={actions}
+        content={this.renderContent()}
+        actions={this.renderActions()}
         onDismiss={() => {
           history.push('/');
         }}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { post: state.posts[ownProps.match.params.id] };
 };
 
-export default PostDelete;
+export default connect(mapStateToProps, { fetchPost })(PostDelete);
